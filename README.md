09b7e0ccffa3a873036f5793264f571cb0ed2ec6
kunuraja <rajkumar.mishra2007@gmail.com>
2022-11-08 18:23:06 +0530

    Spring Primary Bean explained

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # spring-learn-one
 Spring core learning
+
+Primary Bean
+==============
+
+@Primary annotation is used to make the bean primary. Suppose there is conflicts in the bean and we are not using @Qualifier annotation, then the bean annotated 
+with @Primary will be called by default.
+
+And we will get the error "Consider marking one of the beans as @Primary, updating the consumer to accept multiple beans,
+or using @Qualifier to identify the bean that should be consumed"
--- src/main/java/com/raj/springlearnone/SpringLearnOneApplication.java
+++ src/main/java/com/raj/springlearnone/SpringLearnOneApplication.java
@@ -1,6 +1,8 @@
 package com.raj.springlearnone;
 
 import com.raj.springlearnone.controller.MyController;
+import com.raj.springlearnone.controller.MyControllerTwo;
+import com.raj.springlearnone.controller.PrimaryController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -13,6 +15,14 @@ public class SpringLearnOneApplication {
 		MyController myController = context.getBean(MyController.class);
 		String qualifierDemo = myController.demoController();
 		System.out.println(qualifierDemo);
+
+		MyControllerTwo myControllerTwo = context.getBean(MyControllerTwo.class);
+		String demoQualifierPrimary = myControllerTwo.demoControllerTwo();
+		System.out.println(demoQualifierPrimary);
+
+		PrimaryController primaryController = context.getBean(PrimaryController.class);
+		String primaryBeanDemo = primaryController.primaryBeanController();
+		System.out.println(primaryBeanDemo);
 
 	}
 
